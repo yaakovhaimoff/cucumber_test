9d3e76907ba36e8f1a43a39c1abd6843af56b987
Yaakov Haimoff <hyaakov@aol.com>
2023-07-03 10:22:06 +0300

    git init

---
+++ features/step_definitions/first_step.rb
@@ -0,0 +1,23 @@
+Given(/^I click on Got it button$/) do
+  puts("Click on Got it button")
+  find_element(id: "android:id/button1").click
+end
+
+And(/^I see "([^"]*)" in From header$/) do |value|
+  actual_value = find_element(id: "header_text_unit_from").text
+  if(actual_value!=value)
+    fail("Expected value is #{value} but actual value is #{actual_value}")
+  end
+end
+
+And(/^I see "([^"]*)" in To header$/) do |value|
+  actual_value = find_element(id: "header_text_unit_to").text
+  if(actual_value!=value)
+    fail("Expected value is #{value} but actual value is #{actual_value}")
+  end
+end
+
+When(/^I click on Swap button$/) do
+  puts("Click on Swap it button")
+  find_element(id: "fab").click
+end
